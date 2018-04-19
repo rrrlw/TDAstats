dc3a72f3ee50932e08c85aeebd92b4f4807f3dad
Raoul R. Wadhwa <raoulwadhwa@gmail.com>
2018-04-19 10:19:03 -0400

    Added .onUnload for proper C++ DLL cleanup.

--- R/zzz.R
+++ R/zzz.R
@@ -2,3 +2,8 @@
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage("Welcome to TDAstats")
 }
+
+# unload C++ DLL for proper cleanup
+.onUnload <- function (libpath) {
+  library.dynam.unload("TDAstats", libpath)
+}
