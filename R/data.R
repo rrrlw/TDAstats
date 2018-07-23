18824d0b639e44493b0139fc5d226ef1619d9aae
Raoul Wadhwa <raoulwadhwa@gmail.com>
2018-07-23 16:06:46 -0400

    Added documentation for 4 sample datasets added in previous commit.

---
+++ R/data.R
@@ -0,0 +1,35 @@
+#' 2-dimensional point cloud of a unit circle
+#' 
+#' A dataset containing the Cartesian coordinates of 100 points uniformly distributed
+#' on the circumference of a unit circle.
+#' 
+#' @format A matrix with 100 rows and 2 columns: the x- and y-coordinates
+#' @source \url{https://github.com/rrrlw/TDAstats/blob/master/data-raw/circle2d.R}
+"circle2d"
+
+#' 2-dimensional point cloud of a unit square
+#' 
+#' A dataset containing the Cartesian coordinates of 100 points uniformly distributed
+#' within a unit square.
+#' 
+#' @format A matrix with 100 rows and 2 columns: the x- and y-coordinates
+#' @source \url{https://github.com/rrrlw/TDAstats/blob/master/data-raw/unif2d.R}
+"unif2d"
+
+#' 3-dimensional point cloud of a unit cube
+#' 
+#' A dataset containing the Cartesian coordinates of 100 points uniformly distributed
+#' within a unit cube.
+#' 
+#' @format A matrix with 100 rows and 3 columns: the x-, y-, and z-coordinates
+#' @source \url{https://github.com/rrrlw/TDAstats/blob/master/data-raw/unif3d.R}
+"unif3d"
+
+#' 3-dimensional point cloud of a unit sphere
+#' 
+#' A dataset containing the Cartesian coordinates of 100 points uniformly distributed
+#' on the surface of a unit sphere.
+#' 
+#' @format A matrix with 100 rows and 3 columns: the x-, y-, and z-coordinates
+#' @source \url{https://github.com/rrrlw/TDAstats/blob/master/data-raw/sphere3d.R}
+"sphere3d"
