804e3785f9534074fafde191147f1a4f2091f303
Raoul Wadhwa <raoulwadhwa@gmail.com>
2018-08-12 11:07:09 -0400

    Added code coverage for utility.R through visuals.R (test external code, not internal).

--- tests/testthat/test_plots.R
+++ tests/testthat/test_plots.R
@@ -1,6 +1,43 @@
 context("Accurate visualization of known homologies")
 library(TDAstats)
 
+test_that("plot methods detect invalid feature matrices", {
+  # feature matrix with too few columns
+  feature.mat <- matrix(1:6, ncol = 2)
+  expect_error(plot_persist(feature.mat),
+               "Invalid feature matrix with 2 columns: must have exactly 3 columns\\.")
+  
+  # feature matrix with too many columns
+  feature.mat <- matrix(1:8, ncol = 4)
+  expect_error(plot_barcode(feature.mat),
+               "Invalid feature matrix with 4 columns: must have exactly 3 columns\\.")
+  
+  # feature matrix without any rows
+  feature.mat <- matrix(NA, nrow = 0, ncol = 3)
+  expect_error(plot_persist(feature.mat),
+               "Invalid feature matrix: must have at least one row\\.")
+  
+  # feature matrix with first column containing characters
+  feature.mat <- matrix(c("a", 2, 3, "b", 3, 4), ncol = 3, byrow = TRUE)
+  expect_error(plot_barcode(feature.mat),
+               "Invalid feature matrix: first column must be of type integer to represent feature dimension\\.")
+  
+  # feature matrix with second column containing characters (same result as third column containing characters)
+  feature.mat <- matrix(c(0, "a", 2, 1, 2, 3), ncol = 3, byrow = TRUE)
+  expect_error(plot_persist(feature.mat),
+               "Invalid feature matrix: second and third columns must be of type numeric to represent feature birth and death\\.")
+
+  # feature matrix with missing values
+  feature.mat <- matrix(c(0, 1, 2, 1, 2, 3, 1, NA, 4), ncol = 3, byrow = TRUE)
+  expect_error(plot_barcode(feature.mat),
+               "Invalid feature matrix with 1 NAs: should have zero NAs.")
+  
+  # feature matrix with birth before death
+  feature.mat <- matrix(c(0, 0, 1, 0, 0, 2, 1, 2, 1), ncol = 3, byrow = TRUE)
+  expect_error(plot_persist(feature.mat),
+               "Invalid feature matrix: birth must come before death for all features\\.")
+})
+
 test_that("plot_persist produces ggplot object with correct data", {
   # use known circle2d dataset (provided with TDAstats)
   data("circle2d")
