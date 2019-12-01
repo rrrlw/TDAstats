175825feee6f9b81620072bb8e0efee3cc527484
Raoul Wadhwa <raoulwadhwa@gmail.com>
2019-12-01 09:14:30 -0500

    restructured error_check function

--- R/perm_test.R
+++ R/perm_test.R
@@ -5,151 +5,95 @@
 # IMPORT LIBRARIES ----
 library(clue)
 library(rdist)
-library(TDA) # for circleUnif
+library(TDA)        # for circleUnif
 library(data.table) # for as.data.table
 
 # FUNCTIONS ----
 
+#FUNCTION CHECKED (RRRLW)
 error_check <- function(obj1, obj2, iterations,
                         p, q, dim, format, standardize,
                         input_type){
   # function which performs all necessary error checks for the permutation_test_two_groups function
   # each parameter is exactly the same as in the permutation_test_two_groups function
   
+  if (missing(obj1) | missing(obj2)) stop("both parameters must be supplied.")
+  
   if (input_type == "list")
   {
-    if (missing(obj1) | missing(obj2))
-    {
-      return("l1 and l2 must both be supplied.")
-    }
     
-    if((!is.list(obj1) & !is.vector(obj1)) | (!is.list(obj2) & !is.vector(obj2)))
-    {
-      return("l1 and l2 must either be lists or vectors.")
-    }
+    if ((!is.list(obj1) & !is.vector(obj1)) |
+        (!is.list(obj2) & !is.vector(obj2))) stop("l1 and l2 must either be lists or vectors.")
     
-    data_frame_and_matrix_check1 = unlist(lapply(X = 1:length(obj1),FUN = function(X){ 
-      
-      if(!is.data.frame(obj1[[X]]) & !is.matrix(obj1[[X]]))
-      {
-        return(FALSE)
-      }
-      
-      if(nrow(obj1[[X]])<=1 | ncol(obj1[[X]]) == 0)
-      {
-        return(FALSE)
-      }
-      
-      return(TRUE) }))
+    df_mat_check1 <- vapply(X = 1:length(obj1),
+                            FUN.VALUE = logical(1),
+                            FUN = function(i) {
+                              curr_obj <- obj1[[i]]
+                              
+                              if (!is.data.frame(curr_obj) &
+                                  !is.matrix(curr_obj)) return(TRUE)
+                              
+                              if (nrow(curr_obj) <= 1 |
+                                  ncol(curr_obj) == 0) return(TRUE)
+                              
+                              return(FALSE)
+                            })
     
-    data_frame_and_matrix_check2 = unlist(lapply(X = 1:length(obj2),FUN = function(X){ 
-      
-      if(!is.data.frame(obj2[[X]]) & !is.matrix(obj2[[X]]))
-      {
-        return(FALSE)
-      }
-      
-      if(nrow(obj2[[X]])<=1 | ncol(obj2[[X]]) == 0)
-      {
-        return(FALSE)
-      }
-      
-      return(TRUE) }))
+    df_mat_check2 <- vapply(X = 1:length(obj2),
+                            FUN.VALUE = logical(1),
+                            FUN = function(i) {
+                              curr_obj <- obj2[[i]]
+                              
+                              if (!is.data.frame(curr_obj) &
+                                  !is.matrix(curr_obj)) return(TRUE)
+                              
+                              if (nrow(curr_obj) <= 1 |
+                                  ncol(curr_obj) == 0) return(TRUE)
+                              
+                              return(FALSE)
+                            })
     
-    if(length(which(data_frame_and_matrix_check1 == FALSE)) > 0 | length(which(data_frame_and_matrix_check2 == FALSE)) > 0)
-    {
-      return("all list elements must either be multi-row dataframes or multi-row matrices of at least 1 column.")
-    }
-  }else
-  {
-    if(!is.matrix(obj1) & !is.data.frame(obj1))
-    {
-      return("df1 must either be a matrix or a data frame.")
-    }
+    if (any(df_mat_check1) |
+        any(df_mat_check2)) stop("all list elements must either be multi-row dataframes or multi-row matrices of at least 1 column.")
     
-    if(nrow(obj1)<=1 | ncol(obj1)==0)
-    {
-      return("df1 must have at least 2 rows and 1 column.")
-    }
-    if(!is.matrix(obj2) & !is.data.frame(obj2))
-    {
-      return("df2 must be either a matrix or a data frame.")
-    }
+  } else if (input_type == "sample") {
     
-    if(nrow(obj2)<=1 | ncol(obj2)==0)
-    {
-      return("df2 must have at least 2 rows and 1 column.")
-    }
-  }
+    if(!is.matrix(obj1) & !is.data.frame(obj1)) stop("df1 must either be a matrix or a data frame.")
+    
+    if(nrow(obj1) <= 1 | ncol(obj1) == 0) stop("df1 must have at least 2 rows and 1 column.")
+    
+    if(!is.matrix(obj2) & !is.data.frame(obj2)) stop("df2 must be either a matrix or a data frame.")
+    
+    if(nrow(obj2) <= 1 | ncol(obj2) == 0) stop("df2 must have at least 2 rows and 1 column.")
+    
+  } else stop("Invalid input type")
   
-  if(missing(iterations))
-  {
-    return("iterations must be supplied.")
-  }
+  if(missing(iterations)) stop("iterations must be supplied.")
   
-  if(!is.integer(iterations))
-  {
-    return("iterations must be an integer.")
-  }
+  if(!is.integer(iterations) &
+     !is.numeric(iterations)) stop("iterations must be an integer or numeric")
   
-  if(iterations < 1)
-  {
-    return("iterations must be at least 1.")
-  }
+  if(iterations < 1) stop("iterations must be at least 1.")
   
-  if(!is.numeric(p))
-  {
-    return("p must be a number.")
-  }
+  if(!is.numeric(p) |
+     p < 1 |
+     is.infinite(p)) stop("p must be a finite number (at minimum equal to 1)")
   
-  if(p < 1 | is.infinite(p))
-  {
-    return("p must be a finite number at least 1.")
-  }
+  if(!is.numeric(q) |
+     q < 1 |
+     is.infinite(q)) stop("q must be a finite number (at minimum equal to 1)")
   
-  if(!is.numeric(q))
-  {
-    return("q must be a number.")
-  }
+  if(missing(dim)) stop("dim must be supplied.")
   
-  if(q < 1 | is.infinite(q))
-  {
-    return("q must be a finite number at least 1.")
-  }
+  if(!is.integer(dim) |
+     dim < 0 |
+     is.infinite(dim)) stop("dim must be a finite and positive integer.")
   
-  if(missing(dim))
-  {
-    return("dim must be supplied.")
-  }
+  if(!is.character(format)) stop("format must be a character string.")
   
-  if(!is.integer(dim))
-  {
-    return("dim must be an integer.")
-  }
+  if(format %in% c("distmat","cloud") == FALSE) stop("format must either be distmat or cloud.")
   
-  if(dim < 0 | is.infinite(dim))
-  {
-    return("dim must be finite and positive.")
-  }
-  
-  if(!is.character(format))
-  {
-    return("format must be a character string.")
-  }
-  
-  if(format %in% c("distmat","cloud") == F)
-  {
-    return("format must either be distmat or cloud.")
-  }
-  
-  if(!is.logical(standardize))
-  {
-    return("standardize must be either TRUE or FALSE.")
-  }
-  
-  return("No errors :)")
-  
-  
+  if(!is.logical(standardize)) stop("standardize must be either TRUE or FALSE.")
 }
 
 enclosing_radius <- function(X){
@@ -274,12 +218,10 @@ permutation_test_two_groups <- function(l1,l2,iterations,p,q,dim,format,standard
   # dim is the maximum dimension in which to calculate homology for barcodes
   # p is the finite wasserstein distance parameter, p >= 1
   
-  # perform all error checks, return from function if error found
-  e = error_check(obj1 = l1,obj2 = l2,iterations = iterations,p = p,q = q,dim = dim,format = format,standardize = standardize, input_type = "list")
-  if(e != "No errors :)")
-  {
-    stop(e)
-  }
+  # perform all error checks, stop() called from function if error found
+  error_check(obj1 = l1, obj2 = l2, iterations = iterations,
+              p = p, q = q, dim = dim, format = format,
+              standardize = standardize, input_type = "list")
   
   # calculate barcodes for each group
   barcodes1 = lapply(X = l1,FUN = function(X){return(calculate_homology(as.matrix(X), format = format, standardize = standardize, dim = dim,threshold = enclosing_radius(X = X)))})
@@ -334,11 +276,9 @@ permutation_test_two_samples <- function(df1,df2,iterations,p,q,dim,format,stand
   # p is the finite wasserstein distance parameter, p >= 1
   
   # perform all error checks, return from function if error found
-  e = error_check(obj1 = df1,obj2 = df2,iterations = iterations,p = p,q = q,dim = dim,format = format,standardize = standardize, input_type = "sample")
-  if(e != "No errors :)")
-  {
-    stop(e)
-  }
+  error_check(obj1 = df1, obj2 = df2, iterations = iterations,
+              p = p, q = q, dim = dim, format = format,
+              standardize = standardize, input_type = "sample")
   
   # calculate barcode for each dataset
   barcode1 = calculate_homology(as.matrix(df1), format = format, standardize = standardize, dim = dim,threshold = enclosing_radius(X = df1))
